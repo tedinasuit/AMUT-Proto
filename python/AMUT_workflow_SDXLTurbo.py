94ed67ef3768cc4ed78dcb92cc638e51bf3122e1
tedinasuit <73829267+tedinasuit@users.noreply.github.com>
2024-01-16 15:08:26 +0100

    First Prototype: Working
    The web app succesfully sends prompt to ComfyUI and exports a Porsche Poster based on this prompt!

--- python/AMUT_workflow_SDXLTurbo.py
+++ python/AMUT_workflow_SDXLTurbo.py
@@ -1,5 +1,6 @@
 import json
 from urllib import request
+import sys
 
 # This function sends a prompt workflow to the specified URL 
 # (http://127.0.0.1:8188/prompt) and queues it on the ComfyUI server
@@ -14,26 +15,19 @@ def queue_prompt(prompt_workflow):
 # Assign it to the variable prompt_workflow
 prompt_workflow = json.load(open('workflow_AMUT.json'))
 
-# Use this list to set different prompts
-prompt_list = [
-    "enormous sprawling fantasy castle stretching as far as the eye can see, blue moat around castle, road leading to gates medieval architecture, turrets, flags, banners, intricate, mad king ludwig, anime style, 8k resolution, dynamic lighting, hyperdetailed, unreal 5, volumetric lighting, photorealistic, detailed background, digital matte painting, grassy field, small gate",
-    "another prompt here",
-    "yet another prompt",
-    "and one more prompt"
-]
+# Use sys.argv to get the prompt from the command line
+prompt_text = sys.argv[1]
 
 save_image_node = prompt_workflow["27"]
 
-# Update the prompt in the workflow and queue it
-for index, prompt_text in enumerate(prompt_list):
-    # Set the text prompt for positive CLIPTextEncode node
-    prompt_workflow["6"]["inputs"]["text"] = prompt_text
+# Set the text prompt for positive CLIPTextEncode node
+prompt_workflow["6"]["inputs"]["text"] = "Porsche poster," + prompt_text
 
-    # Set filename prefix to be the same as prompt (truncate to first 100 chars if necessary)
-    file_prefix = prompt_text[:100]
+# Set filename prefix to be the same as prompt (truncate to first 100 chars if necessary)
+file_prefix = prompt_text[:100]
 
-    # Update the filename prefix in the SaveImage node
-    prompt_workflow["27"]["inputs"]["filename_prefix"] = file_prefix
+# Update the filename prefix in the SaveImage node
+prompt_workflow["27"]["inputs"]["filename_prefix"] = file_prefix
 
-    # Everything set, add the entire workflow to the queue
-    queue_prompt(prompt_workflow)
+# Everything set, add the entire workflow to the queue
+queue_prompt(prompt_workflow)
--- python/server.py
+++ python/server.py
@@ -1,23 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import subprocess  # Add this line to import subprocess
+import subprocess
 
 app = Flask(__name__)
 CORS(app, origins="http://127.0.0.1:5500")
 
-prompt_list = []
+latest_prompt = ""
 
 @app.route('/add_prompt', methods=['POST'])
 def add_prompt():
+    global latest_prompt
     data = request.get_json()
-    prompt = data.get('prompt')
-    prompt_list.append(prompt)
+    latest_prompt = data.get('prompt')
     return jsonify(success=True)
 
 @app.route('/run_script', methods=['GET'])
 def run_script():
+    global latest_prompt
     # Replace this command with the actual command to run your Python script
-    subprocess.run(['python', 'AMUT_workflow_SDXLTurbo.py'])
+    subprocess.run(['python', 'AMUT_workflow_SDXLTurbo.py', latest_prompt])
     return jsonify(success=True)
 
 if __name__ == '__main__':
